d6e513481124714e478680a4b791a15fac92e52a
Stakhiev-Alexander <sashastakhiev@gmail.com>
2021-08-01 18:45:01 +0300

    Comparison of ssim with different overlap percent

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from glob import glob
 
 import cv2
+import numpy as np
 from skimage.metrics import structural_similarity as ssim
 from tqdm import tqdm
 
@@ -11,20 +12,21 @@ from utils import cut_and_project
 plot = False
 
 if __name__ == '__main__':
-    imgs_num = len(glob('in/*/*/*.png'))
+    imgs_list = glob('in/Fabric1/*/img/*.png')[:200]
+    imgs_num = len(imgs_list)
     print(imgs_num)
-    for nfeatures in range(1100, 3001, 200):
+    for overlap_percent in np.arange(0.02, 0.32, 0.02):
         ssim_sum = 0
         none_num = 0
         start_time = time.time()
 
-        for img_path in tqdm(glob('in/*/*/*.png')):
+        for img_path in tqdm(imgs_list):
 
             orig_img = cv2.imread(img_path)
-            img1, img2 = cut_and_project(orig_img)
+            img1, img2 = cut_and_project(orig_img, overlap_percent=overlap_percent)
 
             result_img = stitch_images(img2, img1, feature_extractor='sift', feature_matching='knn',
-                                       nfeatures=nfeatures)
+                                       nfeatures=5000)
 
             if result_img is None:
                 none_num += 1
